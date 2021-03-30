04d38bfc4cd27207f4c21848dcdb20478d998fa0
koushik-ruidas <koushikruidas@gmail.com>
2021-03-30 15:57:48 +0530

    JDBC connection and executing query

---
+++ src/com/koushik/jdbc/DatabaseConnectionTest.java
@@ -0,0 +1,33 @@
+package com.koushik.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+public class DatabaseConnectionTest {
+	public static void main(String[] args) {
+		String databaseURL = "jdbc:mysql://localhost:3306/Student-Database?useSSL=false&serverTimeZone=UTC";
+		String username = "Student-Database";
+		String password = "koushikRuidas";
+		String sqlstmt = "select * from student where id = ?";
+		int id;
+		System.out.println("Enter the student id: ");
+		Scanner scan = new Scanner(System.in);
+		id = scan.nextInt();
+		try {
+			System.out.println("Connecting to the datbase: " + databaseURL);
+			Connection connection = DriverManager.getConnection(databaseURL, username, password);
+			System.out.println("Connection made successfully");
+			System.out.println("Preaparing a stmt");
+			PreparedStatement select = connection.prepareStatement(sqlstmt);
+			connection.setAutoCommit(false);
+			select.setInt(1, id);
+			ResultSet rs = select.executeQuery();
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
