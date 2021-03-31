65d542fabed950dca2cc766a0aed187134eb8921
koushik-ruidas <koushikruidas@gmail.com>
2021-03-31 14:18:12 +0530

    execute query and sysout the result

---
+++ src/com/koushik/hibernate/ExecuteQuery.java
@@ -0,0 +1,37 @@
+package com.koushik.hibernate;
+
+import java.util.Iterator;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class ExecuteQuery {
+
+	public static void main(String[] args) {
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								.buildSessionFactory();
+		Session session = factory.getCurrentSession();
+		
+		try {
+			session.beginTransaction();
+			
+			List<Student> result = session.createQuery("from Student s "
+					+ "where s.lastName like '%ruid%'").getResultList();
+			
+			Iterator<Student> listIterator = result.iterator();
+			while(listIterator.hasNext()) {
+				System.out.println(listIterator.next().toString());
+			}
+			session.getTransaction().commit();
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			factory.close();
+		}
+	}
+
+}
